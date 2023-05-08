c759a41faa6b9bb97936082bebaf5628e44b9d2e
dbaustin4 <dbuanahagi4@gmail.com>
2023-05-08 18:25:18 +0200

    Line Segment

---
+++ GXPEngine2023/GXPEngine2023/GXPEngine/Physics/LineSegmentSolid.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using GXPEngine;
+using Physics;
+
+
+class LineSegmentSolid : LineSegment
+{
+    ColliderManager engine;
+    LineSegmentCollider lineCollider;
+    BallCollider startCap;
+    BallCollider endCap;
+    public LineSegmentSolid(Vec2 pStart, Vec2 pEnd) : base(pStart, pEnd)
+    {
+        engine = ColliderManager.main;
+        lineCollider = new LineSegmentCollider(this, pStart, pEnd);
+        startCap = new BallCollider(this, 0, pStart);
+        endCap = new BallCollider(this, 0, pEnd);
+        engine.AddSolidCollider(lineCollider);
+        engine.AddSolidCollider(startCap);
+        engine.AddSolidCollider(endCap);
+
+    }
+
+    protected override void OnDestroy()
+    {
+        // Remove the collider when the sprite is destroyed:
+        engine.RemoveSolidCollider(lineCollider);
+    }
+}
