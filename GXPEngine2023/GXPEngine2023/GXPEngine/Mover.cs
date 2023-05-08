bb5d7000cf5fb1e5a89cc2400704850bd07786d9
dbaustin4 <dbuanahagi4@gmail.com>
2023-05-08 17:22:29 +0200

    GxP Folder

---
+++ GXPEngine2023/GXPEngine2023/GXPEngine/Mover.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using GXPEngine;
+
+class Mover : Sprite {
+
+    int moveDir = 1;
+    bool controllable = false;
+
+    float moveSpeed = 2;
+    float turnSpeed = 1;
+
+    public Mover() : base("triangle.png"){
+
+        //SetColor(Utils.Random(0, 1f), (Utils.Random(0, 1f), Utils.Random(0, 1f));
+
+
+    }
+
+    void update() {
+        if (controllable)
+        {
+            if (Input.GetKey(Key.UP)) {
+               Move(0,-moveSpeed);
+            }
+            if (Input.GetKey(Key.DOWN))
+            {
+                Move(0, moveSpeed);
+            }
+            if (Input.GetKey(Key.LEFT))
+            {
+                Move(-turnSpeed, 0);
+            }
+            if (Input.GetKey(Key.RIGHT))
+            {
+                Move(turnSpeed, 0);
+            }
+        } else
+        {
+            Move(0, 2);
+            rotation += moveDir;
+
+            if (Utils.Random(0,100) == 0)
+            {
+                //moveDir = -turnSpeed * moveDir;
+            }
+
+        }
+
+        //reset
+        if (Input.GetKeyDown(Key.SPACE)) {
+            SetXY(game.width / 2, game.height / 2);
+        }
+    }
+}
---
+++ GXPEngine2023/GXPEngine2023/GXPEngine/Obstacle.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using GXPEngine;
+
+class Obstacle{
+    public GameObject obstacle;
+    public float maxX;
+    public float minX;
+    public float maxY;
+    public float minY;
+    public float timeBetweenSpawn;
+    private float spawnTime;
+
+
+    void Update() {
+        Spawn();
+        spawnTime = Time.time + timeBetweenSpawn;
+    
+    }
+
+    void Spawn(){
+        float randomX = Utils.Random(minX, maxX); //similar to unity Random.Range
+        float randomY = Utils.Random(minY, maxY);
+
+        
+    }
+
+
+}
