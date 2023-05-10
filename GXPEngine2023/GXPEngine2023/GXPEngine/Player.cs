acbd7b22cdb11da035408fbef1f4bbb9c7550ff5
dbaustin4 <dbuanahagi4@gmail.com>
2023-05-10 14:17:34 +0200

    Player Movement Fixed

--- GXPEngine2023/GXPEngine2023/GXPEngine/Player.cs
+++ GXPEngine2023/GXPEngine2023/GXPEngine/Player.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using TiledMapParser;
+using Physics;
 
 public class Player : AnimationSprite
 {
@@ -18,11 +19,19 @@ public class Player : AnimationSprite
     public Vec2 velocity;
     Vec2 _position;
     float _speed = 10;
-    //gravity direction
+
+    //gravity direction bools
+    bool bottomGravity = false;
+    bool topGravity = false;
+    bool rightGravity;
+    bool leftGravity;
+
+    //bool[4] gravity = new bool { false, false, false, false }
+
     float speedY = 0;
     float speedX = 0;
     //bool normalGravity = true;
-    public Player(TiledObject tiledObjectPlayer = null) : base("barry.png", 7, 1)
+    public Player(TiledObject tiledObjectPlayer = null) : base("barry.png", 7, 1, -1, false, false)
     {
         if (tiledObjectPlayer != null)
         {
@@ -30,11 +39,15 @@ public class Player : AnimationSprite
             _position.y = tiledObjectPlayer.Y + 32;
         }
         SetCycle(0, 3);
+        _position.RotateDegree(0);
+        bottomGravity = true;
     }
 
     void Update()
     {
+        // Player movement
         velocity.x = 0;
+        velocity.y = 0;
 
         if (Input.GetKey(Key.RIGHT))
         {
@@ -44,73 +57,192 @@ public class Player : AnimationSprite
         {
             velocity.x -= _speed;
         }
+        if (Input.GetKey(Key.UP))
+        {
+            velocity.y -= _speed;
+        }
+        if (Input.GetKey(Key.DOWN))
+        {
+            velocity.y += _speed;
+        }
 
         _position.x += velocity.x;
+        _position.y += velocity.y;
         Movement();
-        Gravity();
-    }
 
-    void Gravity()
-    {
+        // Change gravity direction based on input
+        if (Input.GetKey(Key.W))
+        {
+            bottomGravity = false;
+            topGravity = true;
+            rightGravity = false;
+            leftGravity = false;
+        }
+        else if (Input.GetKey(Key.S))
+        {
+            bottomGravity = true;
+            topGravity = false;
+            rightGravity = false;
+            leftGravity = false;
+        }
+        else if (Input.GetKey(Key.D))
+        {
+            bottomGravity = false;
+            topGravity = false;
+            rightGravity = true;
+            leftGravity = false;
+        }
+        else if (Input.GetKey(Key.A))
+        {
+            bottomGravity = false;
+            topGravity = false;
+            rightGravity = false;
+            leftGravity = true;
+        }
+
         SwitchGravity();
-        //InverseGravity();
+
     }
 
     private void SwitchGravity()
     {
-        if (Input.GetKey(Key.S))
+        float gravityForce = 0.5f;
+
+        if (bottomGravity)
         {
-            speedY = speedY + 1;
-            y = y + speedY;
-            Mirror(false, false);
+            _position.x = x;
+            topGravity = false;
+            rightGravity = false;
+            leftGravity = false;
+            speedY += gravityForce;
+            y += speedY;
+            speedX = 0;
+
+            if (rotation > 0 && rotation < 180)
+            {
+                rotation -= 5;
+
+                if (rotation < 0 || rotation > 360) rotation = 0;
+            }
+            else
+            {
+                rotation += 5;
+
+                if(rotation < 0 || rotation > 360) rotation = 0;
+            }
 
             if (y > 4200 - height)
             {
                 y = 4200 - height;
                 speedY = 0;
-            }
-        } 
-        else if (Input.GetKey(Key.W))
-        {
-            speedY = speedY - 1;
-            y = y - speedY;
-            Mirror(false, true);
-
-            if (y > 3700 + height)
-            {
-                y = 3700 + height;
-                speedY = 0;
+                rotation = 0;
             }
         }
-        else if (Input.GetKey(Key.D))
+        else if (topGravity)
         {
-            speedX = speedX + 1;
-            x = x + speedX;
-            Mirror(true, false);
+            _position.x = x;
+            bottomGravity = false;
+            rightGravity = false;
+            leftGravity = false;
+            speedY -= gravityForce;
+            y += speedY;
+            speedX = 0;
+
+            if (rotation > 180)
+            {
+                rotation -= 5;
+
+                if (rotation < 180) rotation = 180;
+            }
+            else
+            {
+                rotation += 5;
+
+                if (rotation > 180) rotation = 180;
+            }
+
+            if (y < 3700)
+            {
+                y = 3700;
+                speedY = 0;
+                rotation = 180;
+            }
+        }
+        else if (rightGravity)
+        {
+            _position.y = y;
+            bottomGravity = false;
+            topGravity = false;
+            leftGravity = false;
+            speedX += gravityForce;
+            x += speedX;
+            speedY = 0;
+
+            if (rotation > 270 || rotation < 90)
+            {
+                rotation -= 5;
+
+                if (rotation < 270) rotation = 270;
+            }
+            else
+            {
+                rotation += 5;
+
+                if (rotation > 270) rotation = 270;
+            }
 
             if (x > 960 - width)
             {
                 x = 960 - width;
                 speedX = 0;
+                rotation = 270;
             }
         }
-        else if (Input.GetKey(Key.A))
+        else if (leftGravity)
         {
-            speedX = speedX - 1;
-            x = x - speedX;
-            Mirror(false, false);
+            _position.y = y;
+            bottomGravity = false;
+            topGravity = false;
+            rightGravity = false;
+            speedX -= gravityForce;
+            x += speedX;
+            speedY = 0;
 
-            if (x > 60 + width)
+            if (rotation > 90)
             {
-                x = 60 + width;
+                rotation -= 5;
+
+                if (rotation < 90) rotation = 90;
+            }
+            else
+            {
+                rotation += 5;
+
+                if (rotation > 90) rotation = 90;
+            }
+
+            if (x < 128)
+            {
+                x = 128;
                 speedX = 0;
+                rotation = 90;
             }
         }
+
+        if (rotation < 0) rotation = 360 + rotation;
+        if (rotation > 360) rotation = rotation - 360;
     }
 
     void Movement()
     {
-        x = _position.x;
+        if (!leftGravity && !rightGravity)
+        {
+            x = _position.x;
+        }
+        else if (!bottomGravity && !topGravity)
+        {
+            y = _position.y;
+        }
     }
 
     void MoveUntilCollision(GameObject other)
