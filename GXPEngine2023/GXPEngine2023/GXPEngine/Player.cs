fc7232d7bee2cec188f095c4ca577ee3edfec93b
dbaustin4 <dbuanahagi4@gmail.com>
2023-05-10 11:03:58 +0200

    Gravity messy

--- GXPEngine2023/GXPEngine2023/GXPEngine/Player.cs
+++ GXPEngine2023/GXPEngine2023/GXPEngine/Player.cs
@@ -18,8 +18,10 @@ public class Player : AnimationSprite
     public Vec2 velocity;
     Vec2 _position;
     float _speed = 10;
+    //gravity direction
     float speedY = 0;
-    bool normalGravity = true;
+    float speedX = 0;
+    //bool normalGravity = true;
     public Player(TiledObject tiledObjectPlayer = null) : base("barry.png", 7, 1)
     {
         if (tiledObjectPlayer != null)
@@ -34,36 +36,76 @@ public class Player : AnimationSprite
     {
         velocity.x = 0;
 
-        if (Input.GetKey(Key.RIGHT) || (Input.GetKey(Key.D)))
+        if (Input.GetKey(Key.RIGHT))
         {
             velocity.x += _speed;
         }
-        if (Input.GetKey(Key.LEFT) || (Input.GetKey(Key.A)))
+        if (Input.GetKey(Key.LEFT))
         {
             velocity.x -= _speed;
         }
 
-        if (Input.GetKeyDown(Key.SPACE))
-        {
-            speedY = -10;
-        }
-
-        if (normalGravity)
-        {
-            speedY += 1;
-            y += speedY;
-        }
-      
-
-
-
-        /*if(Input.GetKey(Key.G)){
-            _position.y += speedY;
-        }
-        velocity.y += speedY;*/
-
         _position.x += velocity.x;
         Movement();
+        Gravity();
+    }
+
+    void Gravity()
+    {
+        SwitchGravity();
+        //InverseGravity();
+    }
+
+    private void SwitchGravity()
+    {
+        if (Input.GetKey(Key.S))
+        {
+            speedY = speedY + 1;
+            y = y + speedY;
+            Mirror(false, false);
+
+            if (y > 4200 - height)
+            {
+                y = 4200 - height;
+                speedY = 0;
+            }
+        } 
+        else if (Input.GetKey(Key.W))
+        {
+            speedY = speedY - 1;
+            y = y - speedY;
+            Mirror(false, true);
+
+            if (y > 3700 + height)
+            {
+                y = 3700 + height;
+                speedY = 0;
+            }
+        }
+        else if (Input.GetKey(Key.D))
+        {
+            speedX = speedX + 1;
+            x = x + speedX;
+            Mirror(true, false);
+
+            if (x > 960 - width)
+            {
+                x = 960 - width;
+                speedX = 0;
+            }
+        }
+        else if (Input.GetKey(Key.A))
+        {
+            speedX = speedX - 1;
+            x = x - speedX;
+            Mirror(false, false);
+
+            if (x > 60 + width)
+            {
+                x = 60 + width;
+                speedX = 0;
+            }
+        }
     }
 
     void Movement()
